3054eb330c63a50dc0e586500d394c0ce73a2a2d
trizoza <peter.trizuliak@gmail.com>
2022-03-28 12:42:20 +0100

    make my own

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
@@ -13,60 +12,94 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
+        <h1 className={styles.title}>Hello! 👋</h1>
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
+          I'm Peter. Software engineer. Indie maker. Dad. In reverse order of
+          importance.
         </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
+        <p className={styles.description}>
+          I'm to be found{" "}
           <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
+            href="https://twitter.com/trizo_za"
+            target="_blank"
+            rel="noopener noreferrer"
           >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            here
           </a>
-
+          ,{" "}
           <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
+            href="https://www.linkedin.com/in/peter-trizuliak"
+            target="_blank"
+            rel="noopener noreferrer"
           >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            here
           </a>
-        </div>
+          ,{" "}
+          <a
+            href="https://www.instagram.com/trizoza/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            here
+          </a>
+          ,{" "}
+          <a
+            href="https://www.producthunt.com/@parking_pete"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            here
+          </a>
+          ,{" "}
+          <a
+            href="https://github.com/trizoza"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            here
+          </a>
+          ,{" "}
+          <a
+            href="https://trizoza.bio.link/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            here
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://www.instagram.com/something.about.trees/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            here
+          </a>
+          .
+        </p>
+        <p className={styles.description}>
+          I'm building Hacker News for automotive industry{" "}
+          <a
+            href="https://autonews.io/?ref=trizuliak.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            autonews.io
+          </a>
+          , distraction free pomodoro timer{" "}
+          <a
+            href="https://juicytimer.com/?ref=trizuliak.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            juicytimer.com
+          </a>{" "}
+          and few more little projects that are still about to be published.
+        </p>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      {/* <footer className={styles.footer}></footer> */}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
